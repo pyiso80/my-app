246ad7bda34b5368ab2f86cc22af6b6c1d40cc4a
Pyi Soe <pyi.so.80@gmail.com>
2026-03-22 08:37:21 +0630

    added back a quick unit test for the controller

--- src/test/java/me/pyisoe/javatut/myapp/MyAppRestControllerTest.java
+++ src/test/java/me/pyisoe/javatut/myapp/MyAppRestControllerTest.java
@@ -4,8 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.List;
+
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -15,11 +19,16 @@ class MyAppRestControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockitoBean
+    private ContactRepo contactRepo;
+
+    @Test
     void shouldReturnNameFromPostRequest() throws Exception {
         String json = """
             { "name": "Pyi Soe" }
         """;
 
+        given(contactRepo.findAll()).willReturn(List.of(new Contact("Pyi Soe")));
         mockMvc.perform(
                         post("/api")
                                 .contentType(MediaType.APPLICATION_JSON)
