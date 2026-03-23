9df5c8a3800bdcdc59f63d0426bfb37511c534fc
Pyi Soe <pyi.so.80@gmail.com>
2026-03-24 00:58:31 +0630

    form changes (not pass yet)

--- frontend/src/ContactForm.jsx
+++ frontend/src/ContactForm.jsx
@@ -3,15 +3,21 @@ import React, {useRef} from 'react';
 function ContactForm({ onSubmit }) {
     const firstNameRef = useRef(null);
     const lastNameRef = useRef(null);
+    const emailRef = useRef(null);
+    const phoneRef = useRef(null);
 
     const handleSubmit = (e) => {
         e.preventDefault();
         const firstName = firstNameRef.current.value;
         const lastName = lastNameRef.current.value;
+        const phone = phoneRef.current.value;
+        const email = emailRef.current.value;
 
         const formData = {
             firstName,
             lastName,
+            phone,
+            email,
         };
         console.log("Submitted:", formData);
         onSubmit(formData)
@@ -26,6 +32,14 @@ function ContactForm({ onSubmit }) {
             <div>
                 <label>Last Name:</label><br />
                 <input type="text" name="lastName" ref={lastNameRef} />
+            </div>
+            <div>
+                <label>Phone:</label><br />
+                <input type="text" name="phone" ref={phoneRef} />
+            </div>
+            <div>
+                <label>Email:</label><br />
+                <input type="email" name="email" ref={emailRef} />
             </div>
             <button type="submit">Submit</button>
         </form>
