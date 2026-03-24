e42188c7cb2b06afe86639ddf0e3e30fedc0f653
Pyi Soe <pyi.so.80@gmail.com>
2026-03-24 20:42:31 +0630

    configure flyway with spring boot, use postgres both for testing and app running, no more H2

--- src/test/java/me/pyisoe/javatut/myapp/BasicRepositoryTest.java
+++ src/test/java/me/pyisoe/javatut/myapp/BasicRepositoryTest.java
@@ -3,6 +3,7 @@ package me.pyisoe.javatut.myapp;
 import org.jdbi.v3.core.Jdbi;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.context.annotation.Import;
 
@@ -12,6 +13,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @JdbcTest
 @Import({ContactRepo.class, JdbiConfiguration.class })
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class BasicRepositoryTest {
 
     @Autowired
